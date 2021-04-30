98debb12745f51b093db646046f41610b6c63067
Marcos Soares <mavmaso@gmail.com>
2021-04-30 15:54:22 -0300

    clean code, unusable routes/action

--- lib/anais_web/controllers/author_controller.ex
+++ lib/anais_web/controllers/author_controller.ex
@@ -15,7 +15,6 @@ defmodule AnaisWeb.AuthorController do
     with {:ok, %Author{} = author} <- Account.create_author(author_params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", Routes.author_path(conn, :show, author))
       |> render("show.json", author: author)
     end
   end
@@ -23,21 +22,5 @@ defmodule AnaisWeb.AuthorController do
   def show(conn, %{"id" => id}) do
     author = Account.get_author!(id)
     render(conn, "show.json", author: author)
-  end
-
-  def update(conn, %{"id" => id, "author" => author_params}) do
-    author = Account.get_author!(id)
-
-    with {:ok, %Author{} = author} <- Account.update_author(author, author_params) do
-      render(conn, "show.json", author: author)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    author = Account.get_author!(id)
-
-    with {:ok, %Author{}} <- Account.delete_author(author) do
-      send_resp(conn, :no_content, "")
-    end
   end
 end
