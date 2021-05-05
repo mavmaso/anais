22167376270e7353d53bc6436e0f8c3151ce5df6
Marcos Soares <mavmaso@gmail.com>
2021-05-05 13:41:03 -0300

    fix docker-compose

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+# Wait for Postgres to become available.
+until psql -h db -U "postgres" -c '\q' 2>/dev/null; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+echo "\nPostgres is available: continuing with database setup..."
+
+
+# Potentially Set up the database
+mix ecto.create
+mix ecto.migrate
+
+echo "\nTesting the installation..."
+# "Prove" that install was successful by running the tests
+mix test
+
+echo "\n Launching Phoenix web server..."
+# Start the phoenix web server
+mix phx.server
